dd305eaa693c3ac849247a8335f8aa0a5fba5e08
1aaronscott <1aaronscott@users.noreply.github.com>
2020-05-19 16:00:22 -0500

    not in-place method complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,10 +1,28 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+from math import floor
+
+
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    merged_arr = []
+#    merged_arr = [0] * elements
 
     # Your code here
+    # compare first elements of arrA and arrB
+    # remove the smaller from arrA or arrB and put into merged_arr
+    while len(arrA) > 0 and len(arrB) > 0:
+        if arrA[0] >= arrB[0]:
+            merged_arr.append(arrB.pop(0))
+#            print(merged_arr)
+        else:
+            merged_arr.append(arrA.pop(0))
+#            print(merged_arr)
 
+    # handle any leftover list elements
+    while len(arrA) > 0:
+        merged_arr.append(arrA.pop(0))
+    while len(arrB) > 0:
+        merged_arr.append(arrB.pop(0))
 
     return merged_arr
 
@@ -12,22 +30,32 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-
-
+    # deal with lists of one or zero values
+    if len(arr) <= 1:
+        return arr
+    midpoint = floor(len(arr)/2)
+    left = arr[:midpoint]
+    right = arr[midpoint:]
+#    print(left, right)
+    left = merge_sort(left)
+    right = merge_sort(right)
+    arr = merge(left, right)
     return arr
 
 
+print(merge_sort([1, 4, 6, 7, 0, 5, 2, 3, 9, 8]))
+
 # implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
 
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
 
     return arr
 
